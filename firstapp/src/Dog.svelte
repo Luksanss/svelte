b3d768d74586dcc02290c3bd5b15e7c4b63137a8
Lukas Papariga <lukaspapariga@gmail.com>
2023-04-15 18:57:45 +0200

    dog and house components

---
+++ firstapp/src/Dog.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let name;
+</script>
+
+<style>
+    h1 {
+        color: purple;
+    }
+</style>
+
+<h1>The dog name is {name}!</h1>
---
+++ firstapp/src/House.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Dog from './Dog.svelte';
+</script>
+
+<Dog />
